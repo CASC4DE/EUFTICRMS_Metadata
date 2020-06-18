459e624e3a1e056dcdeecf5755dcdecd34dd4143
laura <laura@noemail.net>
2020-06-18 08:19:36 +0000

    renaming some templates update of run.py to prefill the form with uploaded data
    FossilOrigin-Name: 81dcaf49552514c8e2c190b3520efb5f852c311c370bb938ec772c57bbe892d1

--- run.py
+++ run.py
@@ -16,38 +16,32 @@
 
 """
 from __future__ import print_function, division, absolute_import
-"""
 
-
-"""
-import errno, os, sys, json, glob
+import sys, io, pwd, socket, urllib.request, fcntl, re, zipfile, tempfile, subprocess, time, errno, os, json, glob,psutil,calendar
+urlopen = urllib.request.urlopen
+from flask import render_template, flash, redirect, url_for, request, send_from_directory
 op, opd, opb, opj = os.path, os.path.dirname, os.path.basename, os.path.join
-import numpy as np
+from datetime import datetime
+from dateutil.relativedelta import *
 import shutil as sh
-import time
-import subprocess
-import tempfile
-import zipfile
-from fnmatch import fnmatch
-import sys
-import io
-
 import urllib.request
 urlopen = urllib.request.urlopen
-
-import datetime
+import numpy as np
+from numpy import pi
+from sys import platform as _platform
+from fnmatch import fnmatch
+from scipy.constants import N_A as Avogadro
+from scipy.constants import e as electron
+from dateutil.parser import parse
+from werkzeug.urls import url_parse
+from pathlib import Path
 
 from flask import Flask,flash, render_template, request, redirect, url_for, jsonify, send_file,send_from_directory, session
 from werkzeug.utils import secure_filename
-from sys import platform as _platform
-import sys
-sys.path.insert(0, "/home/laura/Repositories/spike")
+
 import spike
 from spike.File import Solarix, Apex
 
-from scipy.constants import N_A as Avogadro
-from scipy.constants import e as electron
-from numpy import pi
 # Global variables
 Debug = False            # Debug Flask
 debug = False
@@ -80,98 +74,52 @@ ALLOWED_EXTENSIONS = set(['mscf'])
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/')
+@app.route('/index')
 def index():
-    '''
-    Main page, loads parameter
-    '''
-    global TMP
-    reduced_params={}
-    if request.method == 'POST':
-        config = request.form.to_dict(flat=False)
-        if 'submit' in config:
-            del config['submit']
-        for element in config:
-            config[element] = str(config[element]).strip("[']")
-        print(config)
-        #config_file = io.open(opj(TMP,'processing_configuration.mscf'), 'w')
-        with open(opj(TMP,'metadata.json'), 'w') as outfile:  
-            json.dump(config, outfile, indent=2)
-        return send_from_directory(directory=TMP, filename="metadata.json",as_attachment=True)
-    else:
-        if len(os.listdir(app.config['UPLOAD_FOLDER'])) != 0:
-            expfold = os.listdir(app.config['UPLOAD_FOLDER'])[0]
-            print("***** EXPFOLD is:",expfold,"*****")
-    #   configfile = 'processing_example.mscf'
-            print("**** reading config file ****")
-            param_file = find_param_file(expfold)
-            if param_file is not None:
-                print('address paramfile is ', param_file)
-                params = Apex.read_param(param_file)
-                with open(param_file) as f: 
-                        lines = f.readlines()
-                spectrometer_type = "Apex"
-                for l in lines:
-                    if "solari" in l:
-                        spectrometer_type = "Solarix"
-                reduced_params = {}
-                reduced_params["name"] = expfold
-                reduced_params['SpectrometerType'] = spectrometer_type
-                reduced_params['EXC_hi'],reduced_params['EXC_low'],reduced_params['SW_h'], reduced_params['TD'],reduced_params['ML1'], reduced_params['ML2'], reduced_params['ML3'], reduced_params['PULPROG'] = params['EXC_hi'], params['EXC_low'],params['SW_h'], params['TD'],params['ML1'], params['ML2'], params['ML3'], params['PULPROG']
-                reduced_params['B0'] = round(float(reduced_params['ML1'])*2*pi/(electron*Avogadro)*1E-3,1)
-                if spectrometer_type == "Solarix":
-                    print("Solarix!")
-                    with open(os.path.join(os.path.dirname(param_file),"ExciteSweep")) as f: 
-                        lines = f.readlines()
-                    NB_step = len(lines[6:])
-                    reduced_params['NBStep'] = NB_step
-                    reduced_params['ExciteSweep1st'] = str(lines[6]).strip('\n')
-                    reduced_params['ExciteSweepLast'] = str(lines[len(lines)-1]).strip('\n')
-                print("Loaded parameters are:", reduced_params)
-                return render_template('config_form.html', param=reduced_params)
-            else:
-                print("PARAM_FILE NONE, using reduced_params.")
-                reduced_params["name"] = "Wrong folder imported, no .m found in dataset:"+str(expfold)
-                pass
-        else:
-            print('NO EXPFOLD TRANSMITTED')
-            reduced_params["name"] = "No folder imported."
-        pre_session = request.form.to_dict(flat=False)
-        for element in pre_session:
-            pre_session[element] = str(pre_session[element]).strip("[']")
-        session = pre_session
-        print(pre_session)
-        print(session)
-        return render_template('config_form.html', param=reduced_params)
+    return redirect(url_for('create_metadata'))
 
-def find_param_file(folder):
-    global UPLOAD_FOLDER
-    F_name = opj(UPLOAD_FOLDER,folder)
+def find_param_file(ExpName):
+    param_file_type = None
+    BaseExpName = ExpName.strip('.')[:-2] 
+    print("BaseExpName:",BaseExpName)
+    working_dir=os.getcwd()
+    print("working dir:",working_dir) 
+    user_UPLOAD_FOLDER = os.path.join(working_dir,'Metadata_Upload_Folder')
+    if not os.path.exists(user_UPLOAD_FOLDER):
+        os.makedirs(user_UPLOAD_FOLDER)
     param_file = None
-    print("F_name:",F_name)
-    for F in os.listdir(F_name):
-        print("F:",F)
-        if F.endswith('.m'):
-            for f in os.listdir(os.path.join(F_name,F)):
-                if f.endswith('.method'):
-                    param_file = os.path.join(F_name,F,f)
+    project_dir = os.path.join(user_UPLOAD_FOLDER,os.listdir(user_UPLOAD_FOLDER)[0])
+    if "{0}_v0.meta".format(BaseExpName) in os.listdir(project_dir):
+        print("There is a meta file!")
+        param_file = os.path.join(project_dir,"{0}_v0.meta".format(BaseExpName))
+        param_file_type = "meta_file"
+    else: #look for .d
+        F = os.path.join(project_dir,"{0}.d".format(BaseExpName))
+        for f in os.listdir(os.path.join(project_dir,F)):
+            if f.endswith('.m'):
+                for ff in os.listdir(os.path.join(project_dir,F,f)):
+                    if ff.endswith('.method'):
+                        param_file = os.path.join(project_dir,F,f,ff)
+                        param_file_type = "brukermethod_file"
     if param_file == None:
         print("No .m folder in .d folder, check your dataset.")
     print("param_file:", param_file)
-    return param_file
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return param_file_type,param_file
 
 @app.route('/import_folder', methods=['GET', 'POST'])
 def import_folder():
-    global UPLOAD_FOLDER
+    working_dir=os.getcwd()
+    print("working dir:",working_dir) 
+    user_UPLOAD_FOLDER = os.path.join(working_dir,'Metadata_Upload_Folder')
+    if not os.path.exists(user_UPLOAD_FOLDER):
+        os.makedirs(user_UPLOAD_FOLDER)
     if request.method == 'POST':
         l = None
-        l = os.listdir(app.config['UPLOAD_FOLDER'])
+        l = os.listdir(user_UPLOAD_FOLDER)
         if l != []:
             print("Content of Upload Folder:",l)
-            to_rm = "rm -rf "+str(opj(app.config["UPLOAD_FOLDER"],l[0]))
+            to_rm = "rm -rf "+str(opj(user_UPLOAD_FOLDER,l[0]))
             print("To RM:",to_rm)
             os.system(to_rm)
         print("Configuration import ...")
@@ -179,16 +127,208 @@ def import_folder():
         for ff in  Files:
             print('uploading',ff.filename )
             direc, name = op.split(ff.filename)
-            os.makedirs(op.join(app.config['UPLOAD_FOLDER'],direc), exist_ok=True)
-            ff.save(op.join(app.config['UPLOAD_FOLDER'],ff.filename))
-        return redirect(url_for('index'))
+            os.makedirs(op.join(user_UPLOAD_FOLDER,direc), exist_ok=True)
+            ff.save(op.join(user_UPLOAD_FOLDER,ff.filename))
+        return redirect(url_for('create_metadata'))
     else:
-        return redirect(url_for('index'))
+        return redirect(url_for('create_metadata'))
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    working_dir=os.getcwd()
+    print("working dir:",working_dir) 
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(user_dir, filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return redirect(url_for('create_metadata'))
+
+@app.route('/select_experiment', methods=['GET', 'POST'])
+def select_experiment():
+    ExpName=None
+    working_dir=os.getcwd()
+    user_UPLOAD_FOLDER = os.path.join(working_dir,'Metadata_Upload_Folder')
+    project_dir = os.path.join(user_UPLOAD_FOLDER,os.listdir(user_UPLOAD_FOLDER)[0])
+    if request.method == 'POST':
+        ExpIndex = request.form['ExpList']
+        print("ExpIndex",ExpIndex)
+        ExpList = []
+        for F in os.listdir(project_dir):
+            if F.endswith('.d'):
+                print("There is a .d folder!")
+                ExpList.append((F))
+        ExpName = ExpList[int(ExpIndex)]
+        print("ExpName selected:", ExpName)
+        return redirect(url_for('create_metadata', ExpName=ExpName))
+    else:
+        return redirect(url_for('create_metadata'))
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def clear(direc):
     if os.path.exists(direc):
         print("Clearing directory:",direc)
         sh.rmtree(direc)
+
+def generate_base_dico(param_file):
+    """
+    reads a *.d Bruker FTICR dataset and generate a dictionary
+    """
+    print("*** Generating base dico ***")
+    # read all params
+    print("*** Param file is:",param_file,"***")
+    params = Apex.read_param(str(param_file))
+    # determine file type
+    with open(param_file) as f: 
+            lines = f.readlines()
+    #print(lines)
+    SpectrometerType = "Apex"
+    for l in lines:
+        if "solari" in l:
+            SpectrometerType = "Solarix"
+
+    # build meta data
+    reduced_params = {}
+    reduced_params['MetaFileType'] = "EUFTICRMS v 1.0"
+    reduced_params['MetaFileVersion'] = "1.0.0"
+    reduced_params['MetaFileCreationDate'] = datetime.now().isoformat()
+
+    reduced_params['FileName'] = str(Path(param_file).parent.parent.parent).split('/')[-1]
+    reduced_params['SpectrometerType'] = SpectrometerType
+    AcquisitionDate = parse(params['CLDATE'])
+    reduced_params['AcqDate'] = AcquisitionDate.strftime('%Y-%m-%d')
+    reduced_params['EndEmbargo'] = (AcquisitionDate + relativedelta(months=18)).strftime('%Y-%m-%d')
+
+    reduced_params['ExcHighMass'] = params['EXC_hi']
+    reduced_params['ExcLowMass'] = params['EXC_low']
+    reduced_params['SpectralWidth'] = params['SW_h']
+    reduced_params['AcqSize'] =  params['TD']
+    reduced_params['CalibrationA'] = params['ML1']
+    reduced_params['CalibrationB'] = params['ML2']
+    reduced_params['CalibrationC'] = params['ML3']
+    reduced_params['PulseProgam'] = params['PULPROG']
+
+    reduced_params['MagneticB0'] = str(round(float(params['ML1'])*2*np.pi/(electron*Avogadro)*1E-3,1))
+    if SpectrometerType == "Solarix":
+        excfile = Path(param_file).parent/"ExciteSweep"
+        print(excfile)
+        if excfile.exists():
+            with open(excfile,'r') as f: 
+                lines = f.readlines()
+            NB_step = len(lines[6:])
+            reduced_params['ExcNumberSteps'] = str(NB_step)
+            reduced_params['ExcSweepFirst'] = str(lines[6]).strip('\n')
+            reduced_params['ExcSweepLast'] = str(lines[len(lines)-1]).strip('\n')
+        else:
+            reduced_params['ExcNumberSteps'] = "NotDetermined"
+            reduced_params['ExcSweepFirst'] = "NotDetermined"
+            reduced_params['ExcSweepLast'] = "NotDetermined"
+    print("Loaded parameters are:", reduced_params)
+    return reduced_params
+
+def generate_reduced_params(param_file, param_file_type, expfold):
+    reduced_params={}
+    print("*** Generating reduced params ***")
+    if param_file is not None and param_file_type=="brukermethod_file":
+        print('address paramfile is ', param_file)
+        reduced_params = generate_base_dico(param_file)
+    elif param_file is not None and param_file_type=="meta_file":
+        with open(param_file) as f:
+            reduced_params = json.load(f)
+        reduced_params['MetaFileEditionDate'] = datetime.now().isoformat()
+    else:
+        print("PARAM_FILE NONE, using reduced_params.")
+        reduced_params["fold_name"] = "Wrong folder imported, no .m found in dataset:"+str(expfold)
+        pass
+    return reduced_params
+
+
+@app.route('/create_metadata/',defaults={'ExpName': None}, methods=['GET','POST'])
+@app.route('/create_metadata/<ExpName>', methods=['GET','POST'])
+def create_metadata(ExpName):
+    '''
+    Metadata form, chdir to the user home directory
+    still some inputs and the output to fix! (specifically dates and imports) 
+    '''
+    global TMP
+    ExpList = []
+    reduced_params={}
+    working_dir=os.getcwd()
+    user_UPLOAD_FOLDER = os.path.join(working_dir,'Metadata_Upload_Folder')
+    if not os.path.exists(user_UPLOAD_FOLDER):
+        os.makedirs(user_UPLOAD_FOLDER)
+    if len(os.listdir(user_UPLOAD_FOLDER)) != 0:
+        project_dir = os.path.join(user_UPLOAD_FOLDER,os.listdir(user_UPLOAD_FOLDER)[0])
+        i = 0
+        for F in os.listdir(project_dir):
+                if F.endswith('.d'):
+                    print("There is a .d folder!")
+                    ExpList.append((i,F))
+                    i+=1
+    print("ExpName:", ExpName)
+    # print("ExpList: ",ExpList)
+    if ExpName is None and ExpList!=[]:
+        ExpName = ExpList[0][1]
+        print("Using default ExpName:", ExpName)
+    if len(os.listdir(user_UPLOAD_FOLDER)) != 0 and ExpName is not None:
+        print("***** Project Dir is:",project_dir,"*****")
+        try:
+            param_file_type, param_file = find_param_file(ExpName)
+            print("param_file_type,param_file",param_file_type,param_file)
+            reduced_params = generate_reduced_params(param_file,param_file_type,project_dir)
+            reduced_params['ExpName'] = ExpName
+            print('fold_name:',project_dir.split('/')[-1])
+            reduced_params['fold_name'] = project_dir.split('/')[-1]
+        except:
+            print('NO EXPFOLD TRANSMITTED')
+            reduced_params["fold_name"] = "The format of the imported folder was not correct, please see the documentation above to get a structure example."
+            reduced_params["ExpName"] = "No experiment can be loaded, please import a correct folder."
+    else:
+        print('NO EXPFOLD TRANSMITTED')
+        reduced_params["fold_name"] = "No folder imported."
+        reduced_params["ExpName"] = "No experiment"
+    if request.method == 'POST':
+        config = request.form.to_dict(flat=False)
+        config['Comment'] = '{}'.format(request.form['Comment'])
+        config['RawPreprocess'] = '{}'.format(request.form['RawPreprocess'])
+        if 'submit' in config:
+            del config['submit']
+        for element in config:
+            config[element] = str(config[element]).strip("[']")
+        meta_json = reduced_params.copy()
+        meta_json.update(config)
+        if 'ExpList' in meta_json:
+            del meta_json['ExpList']
+        if param_file_type=="meta_file":
+            Fname = param_file.split("/")[-1]
+            meta_version = Fname.strip('.meta').split("_")[-1]
+            list_base_filename = Fname.strip('.meta').split("_")
+            list_base_filename = list_base_filename[:-1]
+            base_filename = "_".join(list_base_filename)
+            filename = '{0}_v{1}.meta'.format(base_filename, str(int(meta_version.strip('v'))+1))
+        else:
+            F = os.listdir(user_UPLOAD_FOLDER)[0]
+            print("FileName:",os.path.splitext(F)[0])
+            filename = '{0}_v0.meta'.format(os.path.splitext(F)[0])
+        with open(opj(user_UPLOAD_FOLDER,os.listdir(user_UPLOAD_FOLDER)[0],filename), 'w') as outfile:  
+            json.dump(meta_json, outfile, indent=2)
+        return send_from_directory(directory=opj(user_UPLOAD_FOLDER,os.listdir(user_UPLOAD_FOLDER)[0]), filename=filename, as_attachment=True)
+    else:
+        return render_template('create_metadata.html', param=reduced_params, ExpList=ExpList)
 
 @app.route('/quit')
 def shutdown():
